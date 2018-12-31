64f457f83533945f88821d6d32b9a048e434e829
Jeremy <jeremymiller00@gmail.com>
2018-12-30 20:21:16 -0800

    getting data

--- src/naive_bayes.sh
+++ src/naive_bayes.sh
@@ -6,3 +6,22 @@
 if [ $# -eq 1 ]
     then
     word=$1
+else
+    echo "usage: naive_bayes.sh <word>"
+    exit
+fi
+
+# if the file doesn't exist, download the file
+if ! [ -e enron1.tar.gx ]
+    then
+    wget "http://www.aueb.gr/users/ion/data/enron-spam/preprocessed/enron1.tar.gz"
+fi
+
+# if the directory doesn't exist, uncompress the file
+if ! [ -d enron1 ]
+    then
+    tar zxvf enron1.tar.gz
+
+# change into dir
+cd enron1
+
