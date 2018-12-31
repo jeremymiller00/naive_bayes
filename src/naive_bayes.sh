e4c96068293af95e2f72a9edfd762ad82d6265c3
Jeremy <jeremymiller00@gmail.com>
2018-12-30 20:11:25 -0800

    initial commit

---
+++ src/naive_bayes.sh
@@ -0,0 +1,8 @@
+# A bash script for building a naive bayes text classifier
+
+# Based on Doing Data Science (Schutt and O'Neill, 105)
+
+# how to use the code
+if [ $# -eq 1 ]
+    then
+    word=$1
